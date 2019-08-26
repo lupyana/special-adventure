acc702b8353c6429f160f78dce26d6386ac279cc
Lupyana <lupyanambembati@gmail.com>
2019-08-26 21:36:56 +0300

    Update ui after roll dice action

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var diceImageView1: UIImageView!
     
+    var randomDiceIndex1: Int = 0;
+    var randomDiceIndex2: Int = 0;
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
@@ -20,6 +22,11 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        randomDiceIndex1 = Int.random(in: 1 ... 6)
+        randomDiceIndex2 = Int.random(in: 1 ... 6)
+        
+        diceImageView1.image = UIImage(named: "dice\(randomDiceIndex1)")
+        diceImageView2.image = UIImage(named: "dice\(randomDiceIndex2)")
     }
     
 }
