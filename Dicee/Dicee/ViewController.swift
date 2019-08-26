2e1ce456188e4d1bd196be26ca98cfc60adbe906
Lupyana <lupyanambembati@gmail.com>
2019-08-26 13:36:45 +0300

    Init project
    - Created the project
    - Added image assets
    - Added image views for background and logo

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Lupyana Mbembati on 26/08/2019.
+//  Copyright © 2019 Lupyana Mbembati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
